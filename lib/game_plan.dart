b7503913d82c94351976e71836216248fc5db995
Ellen Zehra <66315528+ezejp20@users.noreply.github.com>
2023-10-27 15:13:06 +0100

    Updated game_plan.dart
    Updated to include player positions.

--- lib/game_plan.dart
+++ lib/game_plan.dart
@@ -6,79 +6,90 @@ class GamePlanPage extends StatelessWidget {
   final int gameDuration;
   final List<PlayerData> players;
 
-  List<PlayerData> rotatedPlayers = [];
-  List<PlayerData> rotatedGoalies = [];
-
-  GamePlanPage(this.gameType, this.gameDuration, this.players) {
-    // Initialize the rotatedPlayers list with a copy of the players list
-    rotatedPlayers = List.from(players);
-    // Initialize the rotatedGoalies list with a copy of the goalies list
-    rotatedGoalies = List.from(players.where((player) => player.isWillingToGoInGoal).toList());
-  }
+  GamePlanPage(this.gameType, this.gameDuration, this.players);
 
   List<PlayerData> onPitchPlayers = [];
   List<PlayerData> inGoalPlayers = [];
   List<PlayerData> offPitchPlayers = [];
 
-List<PlayerData> calculatePlayersForSegment(int segmentNumber) {
-  onPitchPlayers.clear();
-  inGoalPlayers.clear();
-  offPitchPlayers.clear();
+  List<PlayerData> calculatePlayersForSegment(int segmentNumber) {
+    onPitchPlayers.clear();
+    inGoalPlayers.clear();
+    offPitchPlayers.clear();
 
-  // Filter players who are willing to go in goal
-  List<PlayerData> willingGoalies = players.where((player) => player.isWillingToGoInGoal).toList();
+    // Filter players who are willing to go in goal
+    List<PlayerData> willingGoalies = players.where((player) => player.isWillingToGoInGoal).toList();
 
-  // Rotate goalies who are willing to go in goal
-  int goaliesIndex = segmentNumber % willingGoalies.length;
-  inGoalPlayers = [willingGoalies[goaliesIndex]];
+    // Rotate goalies who are willing to go in goal
+    int goaliesIndex = segmentNumber % willingGoalies.length;
+    inGoalPlayers = [willingGoalies[goaliesIndex]];
 
-  // Create a list of outfield players, including those willing to go in goal
-  List<PlayerData> outfieldPlayers = players.where((player) => !inGoalPlayers.contains(player)).toList();
+    // Create a list of outfield players, including those willing to go in goal
+    List<PlayerData> outfieldPlayers = players.where((player) => !inGoalPlayers.contains(player)).toList();
 
-  // Calculate the range of outfield players for this segment
-  int playersOnPitch = gameType - 1;
- // Calculate the outfield players for this segment
-int outfieldStartIndex = segmentNumber % outfieldPlayers.length;
-List<PlayerData> outfieldPlayersForSegment = [];
+    // Calculate the range of outfield players for this segment
+    int playersOnPitch = gameType - 1;
+    int outfieldStartIndex = segmentNumber % outfieldPlayers.length;
+    List<PlayerData> outfieldPlayersForSegment = [];
 
-for (int i = 0; i < playersOnPitch; i++) {
-  int index = (outfieldStartIndex + i) % outfieldPlayers.length;
-  outfieldPlayersForSegment.add(outfieldPlayers[index]);
-}
+    for (int i = 0; i < playersOnPitch; i++) {
+      int index = (outfieldStartIndex + i) % outfieldPlayers.length;
+      outfieldPlayersForSegment.add(outfieldPlayers[index]);
+    }
 
-  // Calculate the list of players substituted off
-  offPitchPlayers = players.where((player) => !inGoalPlayers.contains(player) && !outfieldPlayersForSegment.contains(player)).toList();
+    // Calculate the list of players substituted off
+    offPitchPlayers = players.where((player) => !inGoalPlayers.contains(player) && !outfieldPlayersForSegment.contains(player)).toList();
 
-  // Assign the role for each player
-  outfieldPlayersForSegment.forEach((player) {
-    player.role = 'onPitch';
-  });
-  inGoalPlayers.forEach((player) {
-    player.role = 'inGoal';
-  });
-  offPitchPlayers.forEach((player) {
-    player.role = 'offPitch';
-  });
+    // Assign the role for each player
+    outfieldPlayersForSegment.forEach((player) {
+      player.role = 'onPitch';
+    });
+    inGoalPlayers.forEach((player) {
+      player.role = 'inGoal';
+    });
+    offPitchPlayers.forEach((player) {
+      player.role = 'offPitch';
+    });
 
-  // Combine the lists for this segment
-  List<PlayerData> segmentPlayers = [];
-  segmentPlayers.addAll(outfieldPlayersForSegment);
-  segmentPlayers.addAll(inGoalPlayers);
-  segmentPlayers.addAll(offPitchPlayers);
+    // Combine the lists for this segment
+    List<PlayerData> segmentPlayers = [];
+    segmentPlayers.addAll(outfieldPlayersForSegment);
+    segmentPlayers.addAll(inGoalPlayers);
+    segmentPlayers.addAll(offPitchPlayers);
 
-  return segmentPlayers;
-}
-
+    return segmentPlayers;
+  }
 
   String getPlayersByRole(List<PlayerData> players, String role) {
     List<PlayerData> filteredPlayers = players.where((player) => player.role == role).toList();
-    return filteredPlayers.map((player) => player.name).join(', ');
+
+    if (role == 'onPitch') {
+      // Check if there are players without the necessary positions
+      final missingPositions = ['Defence', 'Mid', 'Forward']
+          .where((position) => filteredPlayers.every((player) => !player.positions.contains(position)))
+          .toList();
+
+      String positionMessage = '';
+      if (missingPositions.isNotEmpty) {
+        // Someone will have to play out of position to cover missing positions
+        positionMessage = 'Someone will have to play out of position to cover ${missingPositions.join('/')}.';
+      }
+
+      return filteredPlayers.map((player) {
+        // Display positions for "Players on the Pitch" without "Goal"
+        final positions = player.positions.where((position) => position != 'Goal');
+        return '${player.name} (${positions.join('/')})';
+      }).followedBy([positionMessage]).join(', '); // Add the message
+    } else {
+      // Do not display positions for "Player in Goal" or "Players Off the Pitch"
+      return filteredPlayers.map((player) => player.name).join(', ');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    final int segmentLength = 5;
-    int numSegments = (segmentLength > 0 && gameDuration >= segmentLength) ? (gameDuration / segmentLength).ceil() : 0;
+    final int numSegments = 10; // Replace with the actual number of segments
+    final int segmentLength = 5; // Replace with the actual segment length
 
     return Scaffold(
       appBar: AppBar(
